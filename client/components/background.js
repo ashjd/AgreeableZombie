0f0672d6de0b8ca433a00a1e07ddcbc32638126e
Ashwini <ashjd29@gmail.com>
2016-06-01 15:43:11 -0700

    capitalize file names

--- client/components/background.js
+++ client/components/background.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 
 var Background = (props) => (
       <div className="background">
+      <img src="../assets/background.jpg"></img>
       </div>
     );
   
