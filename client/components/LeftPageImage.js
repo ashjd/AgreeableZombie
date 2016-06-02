262ab666f59c8d3e6df0d5658c63805451184003
Ashwini <ashjd29@gmail.com>
2016-06-02 12:10:03 -0700

    resolve conflicts

--- client/components/LeftPageImage.js
+++ client/components/LeftPageImage.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 35e7aec95ade54c521645cbe09d65f1d11542d58
 import React, { PropTypes, Component } from 'react';
 
 var LeftPageImage = (props) => (
   <div className="left-page-image"> 
-<<<<<<< HEAD
-    <h2> Left Page Image </h2>
-=======
     <img className="page-image" src={props.bookData[props.pageCounter].image} />
->>>>>>> 35e7aec95ade54c521645cbe09d65f1d11542d58
   </div>  
   );
 
